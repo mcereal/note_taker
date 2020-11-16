55a86ac6033267c7def4cb07e0c0004f38c3d7d3
mcereal <mpbcurtis@gmail.com>
2020-11-16 18:43:33 -0500

    changed port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
